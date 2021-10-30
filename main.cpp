821a4d783e3aa40e720b225ca5538aeb0cf3953a
Ish-D <Ishan.Darji0@gmail.com>
2021-10-29 21:32:29 -0700

    Add progress bar for rendering

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ int main()
     // Render 
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+    // Pixels written out from left to right (i = 0)
+    // Rows top to bottom (j = image_height-1)
     for (int j = image_height-1; j>= 0; --j)
     {
+        // Progress Bar
+        cerr << "\rScanlines remaining: " << j << ' ' << flush;
         for (int i = 0; i<image_width; ++i)
         {
             auto r = double(i)/(image_width-1);
