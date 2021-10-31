8ec534f14343d1d79126e87beaf769177832c67b
Ish-D <Ishan.Darji0@gmail.com>
2021-10-30 22:15:14 -0700

    Added antialiasing, seperated camera into header

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Usings
 using std::shared_ptr;
@@ -18,6 +19,22 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_rad(double degrees) {
     return degrees * pi/180.0;
+}
+
+inline double random_double(){
+    // Return a random number [0,1]
+    return rand()/(RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Return a random number [min, max]
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common headers
