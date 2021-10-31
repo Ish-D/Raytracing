47b6f99bed7efe3b9d212c886bed70ad94ae9341
Ish-D <Ishan.Darji0@gmail.com>
2021-10-30 22:59:02 -0700

    Add gamma correction

--- color.h
+++ color.h
@@ -11,9 +11,9 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel){
 
     //Divide color by number of samples
     auto scale = 1.0/samples_per_pixel;
-    r*=scale;
-    g*=scale;
-    b*=scale;
+    r = sqrt(scale*r);
+    g = sqrt(scale*g);
+    b = sqrt(scale*b);
     
     // Write the translated value of each color component [0,255]
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
