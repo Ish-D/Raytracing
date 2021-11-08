6db1128f5325808e3a8039ca221a96094788f0a4
Ish-D <Ishan.Darji0@gmail.com>
2021-11-08 15:28:55 -0800

    Create final scene
    End of in a week

--- bvh.h
+++ bvh.h
@@ -1,10 +1,11 @@
-#indef BVH_H
+#ifndef BVH_H
 #define BVH_H
 
 #include "rtweekend.h"
 
 #include "hittable.h"
 #include "hittable_list.h"
+#include <algorithm>
 
 class bvh_node : public hittable {
     public:
@@ -14,9 +15,7 @@ class bvh_node : public hittable {
             : bvh_node(list.objects, 0, list.objects.size(), time0, time1)
         {}
 
-        bvh_node(
-            const std::vector<shared_ptr<hittable>>& src_objects,
-            size_t start, size_t end, double time0, double time1);
+        bvh_node(const std::vector<shared_ptr<hittable>>& src_objects, size_t start, size_t end, double time0, double time1);
         
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
@@ -29,23 +28,30 @@ class bvh_node : public hittable {
         aabb box;
 };
 
-bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const {
-    output_box = box;
-    return true;
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis){
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))
+        std::cerr << "No bounding box in bvh_node constructor\n";
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
 }
 
-bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec} const {
-    if (box.hit(r,t_min, t_max))
-        return false;
+inline bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 0);
+}
 
-    bool hit_left = left->hit(r, t_min, t_max, rec);
-    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
+inline bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 1);
+}
 
-    return hit_left || hit_right;
+inline bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 2);
 }
 
 bvh_node::bvh_node(
-    std::vector<shared_ptr<hittable>>& src_objects,
+    const std::vector<shared_ptr<hittable>>& src_objects,
     size_t start, size_t end, double time0, double time1
 ) {
     auto objects = src_objects; // Create a modifiable array of the source scene objects
@@ -80,31 +86,24 @@ bvh_node::bvh_node(
     if (  !left->bounding_box (time0, time1, box_left)
        || !right->bounding_box(time0, time1, box_right)
     )
-        std::cerr << "No bounding box in bvh_node constructor\n";
+        std::cerr << "No bounding box in bvh_node constructor.\n";
 
     box = surrounding_box(box_left, box_right);
 }
 
-inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis){
-    aabb box_a;
-    aabb box_b;
-
-    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))
-        std::cerr << "No bounding box in bvh_node constructor\n";
-
-    return box_a.min().e[axis] < box_b.min().e[axis];
+bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const {
+    output_box = box;
+    return true;
 }
 
-inline bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 0);
-}
+bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    if (box.hit(r,t_min, t_max))
+        return false;
 
-inline bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 1);
-}
+    bool hit_left = left->hit(r, t_min, t_max, rec);
+    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
 
-inline bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 2);
+    return hit_left || hit_right;
 }
 
 #endif
